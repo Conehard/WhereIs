4271e0fb1b592b03c1951b3f6961b1967387033e
André Augusto <theconehard@gmail.com>
2016-11-04 02:35:28 -0200

    Categories finished

--- platforms/android/assets/www/js/catcontroller.js
+++ platforms/android/assets/www/js/catcontroller.js
@@ -1,7 +1,12 @@
 function CategoriesCtrl($scope, $http) {
 	$http.get('http://egendnetwork.com/categorias.php').success(function (data) {
-    $scope.categories = data;
-});
+	    $scope.categories = data;
+	}).error(function (data) {
+	    console.log("Erro na conexão");
+	    
+
+	});
+
 }
 
 angular
