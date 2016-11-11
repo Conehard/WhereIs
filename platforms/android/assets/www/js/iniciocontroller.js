070a60f2ddd0a48838b8a482ca1d22819dde157e
André Augusto <theconehard@gmail.com>
2016-11-11 18:22:16 -0200

    Updates

--- platforms/android/assets/www/js/iniciocontroller.js
+++ platforms/android/assets/www/js/iniciocontroller.js
@@ -4,7 +4,7 @@ function InicioCtrl($scope, $http) {
     $scope.latitude = position.coords.latitude;
     $scope.longitude = position.coords.longitude;
     $scope.latlong = $scope.latitude+','+$scope.longitude;
-    $scope.iframemapa ='https://www.google.com/maps/embed/v1/directions?key=AIzaSyDHPTHqWPfql51BBZv8bobUiRiik05ZT0o&origin='+$scope.latlong+'&destination='+$scope.latlong+'&avoid=tolls|highways';
+    $scope.mapaif = ('https://www.google.com/maps/embed/v1/directions?key=AIzaSyDHPTHqWPfql51BBZv8bobUiRiik05ZT0o&origin='+$scope.latlong+'&destination='+$scope.latlong+'&avoid=tolls|highways');
     $http.get('https://maps.google.com/maps/api/geocode/json?latlng='+$scope.latlong+'&sensor=false&key=AIzaSyD03iJeEEgdhw1O0oTK5qqtd2Iy6uAzJAE').success(function (data) {
         $scope.mapa = data;
         $scope.newmapa = $scope.mapa.results[0].address_components[3].long_name;
