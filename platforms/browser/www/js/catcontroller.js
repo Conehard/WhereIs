ecc844bf2b565891123e230c188572c4e27580db
André Augusto <theconehard@gmail.com>
2016-11-16 16:26:26 -0200

    Updates

--- platforms/browser/www/js/catcontroller.js
+++ platforms/browser/www/js/catcontroller.js
@@ -1,10 +1,12 @@
 function CategoriesCtrl($scope, $http) {
+	$scope.loading = true;
 	$http.get('http://egendnetwork.com/categorias.php').success(function (data) {
 	    $scope.categories = data;
 	}).error(function (data) {
 	    console.log("Erro na conexão");
 	    $scope.erro = "Não foi possível obter as categorias, tenha certeza de estar conectado a internet para utilizar o aplicativo. Caso esteja e ainda esteja vendo este erro, aguarde alguns segundos e tente novamente.";
-
+	}).finally(function () {
+		$scope.loading = false;
 	});
 
 }
