ac4ef072f03a67091ab19e76cb7cfe8366b0cdff
André Augusto <theconehard@gmail.com>
2016-11-16 16:23:04 -0200

    Spin, parameters in url

--- platforms/browser/www/js/alimentacaocontroller.js
+++ platforms/browser/www/js/alimentacaocontroller.js
@@ -1,5 +1,5 @@
 function AlimentacaoCtrl($scope, $http) {
-
+  $scope.loading = true;
   var onSuccess = function(position) {
     $scope.latitude = position.coords.latitude;
     $scope.longitude = position.coords.longitude;
@@ -11,15 +11,32 @@ function AlimentacaoCtrl($scope, $http) {
         $scope.isStatusL = function(com){
 		    return (com.cidade == $scope.newmapa);
 		};
+		$http.get('http://egendnetwork.com/comercios.php?cidade='+$scope.newmapa).success(function (datacomercio) {
+		    $scope.comercio = datacomercio;
+		    $scope.filterSub = { };
+		    $scope.isStatusC = function(com){
+			    return (com.idcategoria == $scope.idcategoria);
+			};
+			$scope.abrirMapa = function (com){
+				$scope.latitudelocal = com.latitude;
+				$scope.longitudelocal = com.longitude;
+	    		var ref = window.open("https://www.google.com.br/maps/dir/'"+$scope.latlong+"'/'"+$scope.latitudelocal+','+$scope.longitudelocal+"'/", '_blank', 'location=no, closebuttoncaption=Voltar');
+	   		};
+		}).error(function (datacomercio) {
+		    console.log("Erro em obter os comercios");
+		    $scope.erro = "Não foi possível obter os comercios, tenha certeza de estar conectado a internet para utilizar o aplicativo. Caso esteja e ainda esteja vendo este erro, aguarde alguns segundos e tente novamente.";
+		}).finally(function () {
+			$scope.loading = false;
+	    });
     });
   };
 
-  function onError(error) {
-      alert('code: '    + error.code    + '\n' +
-            'message: ' + error.message + '\n');
-  };
+	function onError(error) {
+		alert('code: '    + error.code    + '\n' +
+        	  'message: ' + error.message + '\n');
+	};
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
 
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	$http.get('http://egendnetwork.com/categorias.php').success(function (data) {
 	    $scope.categories = data;
 	    $scope.idcategoria = 1;
@@ -29,32 +46,7 @@ navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	}).error(function (data) {
 	    console.log("Erro na conexão categorias");
 	    $scope.erro = "Não foi possível obter as categorias, tenha certeza de estar conectado a internet para utilizar o aplicativo. Caso esteja e ainda esteja vendo este erro, aguarde alguns segundos e tente novamente.";
-
 	});
-
-	$http.get('http://egendnetwork.com/comercios.php').success(function (datacomercio) {
-	    $scope.comercio = datacomercio;
-	    $scope.filterSub = { };
-	    $scope.isStatusC = function(com){
-		    return (com.idcategoria == $scope.idcategoria);
-		};
-		$scope.abrirMapa = function (com){
-			$scope.latitudelocal = com.latitude;
-			$scope.longitudelocal = com.longitude;
-
-
-    		var ref = window.open("https://www.google.com.br/maps/dir/'"+$scope.latlong+"'/'"+$scope.latitudelocal+','+$scope.longitudelocal+"'/", '_blank', 'location=no, closebuttoncaption=Voltar');
-   		};
-		
-		
-	}).error(function (datacomercio) {
-	    console.log("Erro em obter os comercios");
-	    $scope.erro = "Não foi possível obter os comercios, tenha certeza de estar conectado a internet para utilizar o aplicativo. Caso esteja e ainda esteja vendo este erro, aguarde alguns segundos e tente novamente.";
-
-	});
-
-	
-	
 };
 
 angular
